3a5ecd78851445de64b2a77ae3d19130d518e1f7
Christopher Yee Mon <christopher.yeemon@gmail.com>
2013-09-08 20:54:45 -0400

    chapter 3.03: edited the secret_token.rb to include the random secure_token generation

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,19 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SampleAppRails4Progress::Application.config.secret_key_base = '2b79647ba28a5b7c2214f69e188c707a211f473521704a572d2884d477ce9b472dce0d464e827049027eb7c6f04a73b89250da6b0339b7ae408c382df9144ce0'
+#SampleAppRails4Progress::Application.config.secret_key_base = '2b79647ba28a5b7c2214f69e188c707a211f473521704a572d2884d477ce9b472dce0d464e827049027eb7c6f04a73b89250da6b0339b7ae408c382df9144ce0'
+
+def secure_token
+  token_file = Rails.root.join('secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+SampleAppRails4Progress::Application.config.secret_key_base = secure_token

8c36397e1629ebf514f4452d224536125433ba60
Christopher Yee Mon <christopher.yeemon@gmail.com>
2013-09-08 20:24:21 -0400

    chapter 3.01: rails new sample_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleAppRails4Progress::Application.config.secret_key_base = '2b79647ba28a5b7c2214f69e188c707a211f473521704a572d2884d477ce9b472dce0d464e827049027eb7c6f04a73b89250da6b0339b7ae408c382df9144ce0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppRails4Progress::Application.config.session_store :cookie_store, key: '_sample_app_rails_4_progress_session'

8fbee84048032ebe894c84a16c070f1e20b89946
Christopher Yee Mon <christopher.yeemon@gmail.com>
2013-09-09 19:13:04 -0400

    chapter 3.08: first test added to rspec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,13 @@ require 'spec_helper'
 
 describe "StaticPages" do
   
+  describe "Home page" do
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+  
   describe "GET /static_pages" do
     it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers

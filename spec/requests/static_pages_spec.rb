1a37d7f9c66b8124678b748fa0448b92f8312827
Christopher Yee Mon <christopher.yeemon@gmail.com>
2013-09-24 20:39:53 -0400

    chapter 3.12: added the changes needed for the about page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -16,4 +16,11 @@ describe "StaticPages" do
     end
   end
   
+  describe "About page" do
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+  
 end
